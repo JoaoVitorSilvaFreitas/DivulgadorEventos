2d993f6ea5119874ebb1059b441d97e02d78e3bd
Joao VItor <joaovitordasilvafreitas@gmail.com>
2017-12-21 14:19:11 -0200

    Commit inicial

---
+++ app/src/main/java/com/example/joaovitor/divulgadoreventos/Fragmentos/telaMaps.java
@@ -0,0 +1,150 @@
+package com.example.joaovitor.divulgadoreventos.Fragmentos;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.example.joaovitor.divulgadoreventos.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class telaMaps extends FragmentActivity implements OnMapReadyCallback {
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    // Valor booleano nulo para verificar a permissão.
+    private boolean PermissaoLocalizacao;
+    private Location UltimaPosicao;
+    private GoogleMap mMap;
+
+    //abre o mapa com um marcador no sesc.
+    private void PositionSesc() {
+        LatLng SescSjc = new LatLng(-23.200979, -45.892457);
+        mMap.addMarker(new MarkerOptions().position(SescSjc).title("Sesc"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SescSjc));
+        //zoom no sesc
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(SescSjc, 18);
+        mMap.moveCamera(update);
+        //tipo de mapa
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        VerificaPermissao();
+        UpdatePermissao();
+    }
+
+    private void PositionSesi() {
+        LatLng SesiSjc = new LatLng(-23.248826, -45.884914);
+        mMap.addMarker(new MarkerOptions().position(SesiSjc).title("Teatro Sesi"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SesiSjc));
+        //zoom no Teatro sesi
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(SesiSjc, 18);
+        mMap.moveCamera(update);
+        //tipo de mapa
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        //Verificar e ativar a permissão do usuário
+        VerificaPermissao();
+        UpdatePermissao();
+    }
+
+    private void PositionSjc() {
+        // Adiciona a marca
+        LatLng Sjk = new LatLng(-23.215932, -45.895167);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Sjk));
+        //zoom no sesc
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(Sjk, 15);
+        mMap.moveCamera(update);
+        //tipo de mapa
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        //controle de zoom
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        //Verificar e ativar a permissão do usuário
+        VerificaPermissao();
+        UpdatePermissao();
+    }
+
+    //Função responsável para verificar a permissão, caso não esteja ativa ele requisita a
+    // autorização do usuário
+    private void VerificaPermissao() {
+        //Verifica se a permissao esta ativa.
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            PermissaoLocalizacao = true;
+        } else {
+            // Caso nao esteja, o sera aberta uma tela para o usuario permitir
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    //função que verifica a permissão de mapa ativa no aplicativo,
+    //responsavel para abrir a localização atual.
+    private void UpdatePermissao() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            //caso a permissao esteja ativa, abrir os botoes de localizacao.
+            if (PermissaoLocalizacao) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                // caso nao esteja ativa, o usuario tera que permitir o acesso ao GPS,
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                //UltimaPosicao = null;
+                VerificaPermissao();
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_maps);
+        // Cria o fragmento de mapa, para executar o map.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        //Funcao bundle, recebendo o conteudo da intent que esta na tela main,
+        // verificando qual botao esta sendo pressionado atraves de um "codigo" em String.
+        Bundle bundle = getIntent().getExtras();
+        if (bundle.containsKey("FloatingButton")) {
+            PositionSjc();
+        } else if (bundle.containsKey("Sesc")) {
+            PositionSesc();
+        } else if (bundle.containsKey("Sesi")) {
+            PositionSesi();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        PermissaoLocalizacao = false;
+        //Switch checando se a permissao ao acesso da localizaçao esta ativo atraves do valor INT da constante.
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // Se a requisiçao for cancelada, os arrays de resultado estarao vazios.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    PermissaoLocalizacao = true;
+                }
+            }
+        }
+        // Checar permissao, abrir o botao de verificar posicao atual.
+        UpdatePermissao();
+    }
+
+}
---
+++ app/src/main/java/com/example/joaovitor/divulgadoreventos/Interfaces/telaMain.java
@@ -0,0 +1,170 @@
+package com.example.joaovitor.divulgadoreventos.Interfaces;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.joaovitor.divulgadoreventos.Fragmentos.telaMaps;
+import com.example.joaovitor.divulgadoreventos.R;
+
+public class telaMain extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    //intent para abrir a posiçao do sesi
+    public void SesiIntent() {
+        Intent itsesi = new Intent(telaMain.this, telaMaps.class);
+        itsesi.putExtra("Sesi", 2);
+        startActivity(itsesi);
+    }
+    //intent para abrir a posição do sesc
+    public void SescIntent() {
+        Intent itsesc = new Intent(telaMain.this, telaMaps.class);
+        itsesc.putExtra("Sesc", 1);
+        startActivity(itsesc);
+    }
+    //Intent que mostra uma mensagem.
+    private void InformacoesAlert(){
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+        dialog.setIcon(R.drawable.ic_menu_manage);
+        dialog.setTitle("Informaçoes do app");
+        dialog.setMessage("App em desenvolvimento por: Joao Vitor," +
+                " com a finalidade de divulgar eventos culturais na cidade de São josé dos campos");
+        dialog.setCancelable(false);
+
+        dialog.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        dialog.show();
+    }
+    //intent para startar a tela de configurações
+    private void Configs(){
+        Intent itconfig = new Intent(telaMain.this,telaConfiguracoes.class);
+        startActivity(itconfig);
+    }
+
+
+    private FloatingActionButton Fab_Map;
+    private Toolbar tBar_Tool;
+    private DrawerLayout Lay_Draw;
+    private NavigationView Lay_NavView;
+    private Spinner Spn_Cidade;
+    //Array para ser inserido dentro do Spinner de cidades.
+    private String[] cidadesAdap = new String[]{"São José dos Campos"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_main);
+
+        Lay_Draw = (DrawerLayout) findViewById(R.id.Lay_Draw);
+        tBar_Tool = (Toolbar) findViewById(R.id.tBar_Tool);
+        setSupportActionBar(tBar_Tool);
+        Lay_NavView = (NavigationView) findViewById(R.id.Lay_NavView);
+        Spn_Cidade = (Spinner) findViewById(R.id.Spn_Cidade);
+
+        // FloatingButton para abrir na cidade com possibilidades de zoom e
+        // posicionamento atual.
+        Fab_Map = (FloatingActionButton) findViewById(R.id.Fab_Map);
+        Fab_Map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view == Fab_Map) {
+                    Intent intent = new Intent(telaMain.this, telaMaps.class);
+                    intent.putExtra("FloatingButton", 0);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, Lay_Draw, tBar_Tool, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        //Layout do navigationDrawer
+        Lay_Draw.addDrawerListener(toggle);
+        toggle.syncState();
+        Lay_NavView.setNavigationItemSelectedListener(this);
+
+        ArrayAdapter<String> CityAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, cidadesAdap);
+        Spn_Cidade.setAdapter(CityAdapter);
+        Spn_Cidade.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (Lay_Draw.isDrawerOpen(GravityCompat.START)) {
+            Lay_Draw.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.tela_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (item.getItemId()) {
+            case R.id.nav_sesc:
+                SescIntent();
+                break;
+            case R.id.nav_sesi:
+                SesiIntent();
+                break;
+            case R.id.nav_config:
+                Configs();
+                break;
+            case R.id.nav_info:
+                InformacoesAlert();
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.Lay_Draw);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
